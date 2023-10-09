0b8f8e03af42199a3c9c57431e5db633534ee723
Skey <chenskey8@gmail.com>
2023-10-09 15:00:53 +0800

    修改路徑

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -17,7 +17,7 @@ router.post('/login', loginController)
 router.get("/test", requireSignIn, isAdmin, testController);
 
 //protected route auth
-router.get('user-auth', requireSignIn, (req, res) => {
+router.get('/user-auth', requireSignIn, (req, res) => {
     res.status(200).send({ ok: true });
 })
 
