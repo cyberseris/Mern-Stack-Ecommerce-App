5c5d1e6cc99073bbdc003afdf0e4bd7628a6673f
Skey <chenskey8@gmail.com>
2023-10-11 11:35:02 +0800

    新增商品、取得商品功能

---
+++ controllers/productController.js
@@ -0,0 +1,266 @@
+import productModel from "../models/productModel.js";
+/* import categoryModel from "../models/categoryModel.js";
+import orderModel from "../models/orderModel.js"; */
+
+import fs from "fs";
+import slugify from "slugify";
+import braintree from "braintree";
+import dotenv from "dotenv";
+import categoryModel from "../models/categoryModel.js";
+
+dotenv.config();
+
+//payment gateway
+/* var gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+}); */
+
+
+//create category
+export const createProductController = async (req, res) => {
+    try {
+        const { name, description, price, category, quantity, shipping } = req.fields;
+        const { photo } = req.files;
+
+        switch (true) {
+            case !name:
+                return res.status(500).send({ error: "Name is Required" });
+            case !description:
+                return res.status(500).send({ error: "Description is Required" });
+            case !price:
+                return res.status(500).send({ error: "Price is Required" });
+            case !category:
+                return res.status(500).send({ error: "Category is Required" });
+            case !quantity:
+                return res.status(500).send({ error: "Quantity is Required" });
+            case photo && photo.size > 1000000:
+                return res.status(500).send({ error: "photo is Required and should be less then 1mb" })
+        }
+
+        const products = new productModel({ ...req.fields, slug: slugify(name) });
+        if (photo) {
+            products.photo.data = fs.readFileSync(photo.path);
+            products.photo.contentType = photo.type;
+        }
+
+        await products.save();
+        res.status(201).send({
+            success: true,
+            message: "Product Created Successfully",
+            products,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error in creating product",
+        })
+    }
+};
+
+//get all products
+export const getProductController = async (req, res) => {
+    try {
+        const products = await productModel
+            .find({})
+            .populate("category")
+            .select("-photo")
+            .limit(12)
+            .sort({ createdAt: -1 });
+
+        res.status(200).send({
+            success: true,
+            countTotal: products.length,
+            message: "AllProducts",
+            products,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in getting products",
+            error: error.message,
+        });
+    }
+};
+
+//get single products
+export const getSingleProductController = async (req, res) => {
+    try {
+        const product = await productModel
+            .findOne({ slug: req.params.slug })
+            .select("-photo")
+            .populate("category");
+        res.status(200).send({
+            success: true,
+            message: "Single Product",
+            product,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in getting single product",
+            error,
+        });
+    }
+};
+
+//get photo 
+export const productPhotoController = async (req, res) => {
+    try {
+        const product = await productModel
+            .findById(req.params.pid)
+            .select("photo")
+        if (product.photo.data)
+            res.set("Content-type", product.photo.contentType);
+        return res.status(200).send(product.photo.data);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while getting photo",
+            error,
+        });
+    }
+};
+
+//delete product
+export const deleteProductController = async (req, res) => {
+    try {
+        const product = await productModel
+            .findByIdAndDelete(req.params.pid).select("-photo");
+        res.status(200).send({
+            success: true,
+            message: "Product Deleted successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while deleting product",
+            error,
+        });
+    }
+};
+
+//update product
+export const updateProductController = async (req, res) => {
+    try {
+        const { name, description, price, category, quantity, shipping } = req.fields;
+        const { photo } = req.files;
+
+        switch (true) {
+            case !name:
+                return res.status(500).send({ error: "Name is Required" });
+            case !description:
+                return res.status(500).send({ error: "Description is Required" });
+            case !price:
+                return res.status(500).send({ error: "Price is Required" });
+            case !category:
+                return res.status(500).send({ error: "Category is Required" });
+            case !quantity:
+                return res.status(500).send({ error: "Quantity is Required" });
+            case photo && photo.size > 1000000:
+                return res.status(500).send({ error: "photo is Required and should be less then 1mb" })
+        }
+
+        const products = await productModel.findByIdAndUpdate(
+            req.params.pid,
+            { ...req.fields, slug: slugify(name) },
+            { new: true }
+        );
+        if (photo) {
+            products.photo.data = fs.readFileSync(photo.path);
+            products.photo.contentType = photo.type;
+        }
+
+        await products.save();
+        res.status(201).send({
+            success: true,
+            message: "Product Updated Successfully",
+            products,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error in update product",
+        })
+    }
+};
+
+//filters
+export const productFiltersController = async (req, res) => {
+    try {
+        const { checked, radio } = req.body;
+        let args = {};
+        if (checked.length > 0) args.category = checked;
+        const products = await productModel.find(args);
+        return res.status(200).send({
+            success: true,
+            products,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            success: false,
+            message: "Error While Filtering Products",
+            error,
+        });
+    }
+};
+
+//product count
+export const productCountController = async (req, res) => {
+    try {
+        // 
+        const total = await productModel.find({}).estimatedDocumentCount();
+        let args = {};
+        res.status(200).send({
+            success: true,
+            total,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            success: false,
+            message: "Error While Filtering Products",
+            error,
+        });
+    }
+};
+
+//product count
+export const productListController = async (req, res) => {
+    try {
+        // 
+        const perPage = 6;
+        const page = req.params.page ? req.params.page : 1;
+        const products = await productModel
+            .find({})
+            .select("-photo")
+            .skip((page - 1) * perPage)
+            .limit(perPage)
+            .sort({ createAt: -1 });
+        res.status(200).send({
+            success: true,
+            products,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            success: false,
+            message: "Error in per page",
+            error,
+        });
+    }
+};
+
+//待...
+
---
+++ routes/productRoutes.js
@@ -0,0 +1,70 @@
+import express from 'express'
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable"; //檔案處理專家
+import {
+    /* brainTreePaymentController,
+    brainTreeTokenController, */
+    createProductController,
+    deleteProductController,
+    getProductController,
+    getSingleProductController,
+    /* productCategoryController, */
+    productCountController,
+    productFiltersController,
+    productListController,
+    productPhotoController,
+    /* relatedProductController,
+    searchProductController, */
+    updateProductController,
+} from "../controllers/productController.js";
+
+
+//router object
+const router = express.Router();
+//routing
+//1.server.js => 2. productRoutes 3. productController.js => 4. authMiddleware.js
+//create product
+router.post("/create-product", requireSignIn, isAdmin, formidable(), createProductController);
+
+//get products
+router.get("/get-product", getProductController);
+
+//update product
+router.put("/update-product/:id", requireSignIn, isAdmin, formidable(), updateProductController);
+
+
+//single product
+router.get("/single-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete("/delete-product/:pid", requireSignIn, isAdmin, deleteProductController);
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.post("/product-count", productCountController);
+
+//product per page
+router.post("/product-list/:page", productListController);
+
+//search product
+/* router.post("/search/:keyword", searchProductController); */
+
+//similar product
+/* router.post("/related-product/:pid/:cid", relatedProductController); */
+
+//category wise product
+/* router.post("/product-category/:slug", productCategoryController); */
+
+//payments routes
+//token
+/* router.post("/braintree/token", brainTreeTokenController); */
+
+//payments
+/* router.post("/braintree/payment", requireSignIn, brainTreePaymentController); */
+
+export default router;
